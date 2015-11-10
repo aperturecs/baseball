faab87c7866fe2eafe97999bec9debaaddcce262
Mase <leynuxms@gmail.com>
2015-11-10 22:32:02 +0900

    update

--- hitterProfileParser.py
+++ hitterProfileParser.py
@@ -37,4 +37,4 @@ def hitterProfileParsing(player):
             t = 0
         data.append(t)
 
-        #player.pprofile.append(PitcherProfile(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15], data[16], data[17], data[18], data[19], data[20], data[21], data[22], data[23]))
+        #player.pprofile.append(PitcherProfile(player.playerId,data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15], data[16], data[17], data[18], data[19], data[20], data[21], data[22], data[23]))
