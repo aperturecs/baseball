fab14eb9362d903b004a2aab660ff53db5b6fad3
codertimo <codertimo@gmail.com>
2015-11-12 11:56:15 +0900

    query result update

---
+++ insertQuery.py
@@ -0,0 +1,9 @@
+from query import Query
+
+f = open("sql.txt","r")
+lines = f.readlines()
+query = Query()
+
+for line in lines:
+    line = line.split("\n")[0]
+    query.quering_add(line)
--- main.py
+++ main.py
@@ -12,29 +12,35 @@ import playerParser
 from player import Player
 from playerCode import PlayerCode
 
+f = open("sql.txt","w")
 codes = PlayerCode()
 # query = Query()
 
+count = 1
+
 for code in codes.codes:
     player = playerParser.playerParsing(code)
-    print player.addQuery()
     if player.type =="Hitter":
         hitterProfileParser.hitterProfileParsing(player)
         hitterGameParser.hitterGameParsing(player)
         # query.quering_add(player.addQuery())
         # query.quering_add(player.profile.addQuery())
-        print player.profile.addQuery()
+        f.write(player.addQuery()+"\n")
+        f.write(player.profile.addQuery()+"\n")
         for game in player.hitterGames:
             # query.quering_add(game.addQuery())
-             print game.addQuery()
+            f.write(game.addQuery()+"\n")
+
 
     else:
         pitcherProfileParser.pitcherProfileParsing(player)
         pitcherGameParser.pitcherGameParsing(player)
         # query.quering_add(player.addQuery())
         # query.quering_add(player.profile.addQuery())
-        print player.profile.addQuery()
+        f.write(player.addQuery()+"\n")
+        f.write(player.profile.addQuery()+"\n")
         for game in player.pitcherGames:
             #query.quering_add(game.addQuery())
-             print game.addQuery()
-    print "이름 :"+ player.name + " 팀 :"+player.team  +" 타입 :"+player.type
+            f.write(game.addQuery()+"\n")
+    print "["+str(count)+"/"+str(len(codes.codes))+"] 이름 :"+ player.name + " 팀 :"+player.team  +" 타입 :"+player.type
+    count +=1
