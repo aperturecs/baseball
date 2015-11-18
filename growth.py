5e903dd140ef05b90dfd30bb6eb9d7ea2c3940ce
hyemdooly <hy3m0223@gmail.com>
2015-11-18 20:15:34 +0900

    commit

--- growth.py
+++ growth.py
@@ -7,12 +7,31 @@ y = an-a1
 '''
 
 
-OPS = [] #매 경기 OPS
+
+
+# 1. 모든 게임 정보
+#     -> 디비에서 긁어오고, 클래스화 시키고, 넘기기
+# 2. 월단위로 줄이고
+#     -> 다 더하기
+# 3. 성장률 구하기
+#     -> 알고리즘 돌리기
+
+Games = []
+
+
+
+OBP = (H + BB + HBP)/(AB + BB + SF + HBP)
+SLG = TB/AB
+OPS = OBP + SLG
+
+
+
+OPS_A = [] #매 경기 OPS
 ops_y =[] #OPS 성장률 y값
 OPS_f = OPS[0] #맨 처음 경기 OPS
 
 for i in OPS:
-    OPS_growth = OPS[i] - OPS_f #한 경기 OPS - 맨 처음경기 OPS
+    OPS_growth = OPS_A[i] - OPS_f #한 경기 OPS - 맨 처음경기 OPS
     ops_y.append(OPS_growth) #ops_y 리스트에 추가
 
 
