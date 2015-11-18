c452ad496747a0094d0ac438efef222cd1925289
codertimo <codertimo@gmail.com>
2015-11-18 20:32:16 +0900

    commit

--- hitterGame.py
+++ hitterGame.py
@@ -1,12 +1,13 @@
 import json
 class HitterGame():
 
-    def __init__(self, playerId, year, date, sideTeam, AVG1, AB, R, H, B2, B3, HR, RBI, SB, CS, BB, HBP, SO, GDP, AVG2):
+    # def __init__(self, playerId, year, date, sideTeam, AVG1, AB, R, H, B2, B3, HR, RBI, SB, CS, BB, HBP, SO, GDP, AVG2):
+    def __init__(self, playerId, year, month,day, sideTeam, AVG1, AB, R, H, B2, B3, HR, RBI, SB, CS, BB, HBP, SO, GDP, AVG2):
         self.playerId = playerId
         self.type= "Hitter"
         self.year = year
-        self.month = int(date.split(".")[0])
-        self.day = int(date.split(".")[1])
+        self.month = month
+        self.day = day
         self.sideTeam = sideTeam
         self.AVG1 = AVG1
         self.AB = AB
