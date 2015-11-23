b3473bbb9998307f0cd4759ca6c731325385cfb6
codertimo <codertimo@gmail.com>
2015-11-24 00:21:41 +0900

    Error Fix Dict Type Method Get()

--- growth.py
+++ growth.py
@@ -28,18 +28,18 @@ OPS_A = [] #매월 경기 OPS
 for year in range(2010, 2015):
     for month in range(3, 10):
         for i in Games:
-            if Games[i]["year"]==year and Games[i]["month"]==month:
-                OBP = (Games[i]["H"] + Games[i].["BB"] + Games[i].["HBP"])/(Games[i]["AB"] + Games[i]["BB"] + Games[i]["HBP"])
-                SLG = Games[i]["TB"]/Games[i]["AB"]
+            if Games[i].get("year")==year and Games[i].get("month")==month:
+                OBP = (Games[i].get("H") + Games[i].get("BB") + Games[i].get("HBP"))/(Games[i].get("AB") + Games[i].get("BB") + Games[i].get("HBP"))
+                SLG = Games[i].get("TB")/Games[i].get("AB")
                 OPS = OBP + SLG
                 OPS_A.append({"year":year, "month":month, "ops":OPS}) #계산한 OPS 매월별 집어넣기
 
 OPS_y =[] #OPS 성장률 y값
-OPS_f = OPS_A[0]["ops"] #맨 처음 경기 OPS
+OPS_f = OPS_A[0].get("ops") #맨 처음 경기 OPS
 
 for i in OPS_A:
-    OPS_growth = OPS_A[i]["ops"] - OPS_f #한 경기 OPS - 맨 처음경기 OPS
-    ops_y.append({"year":OPS_A[i]["year"], "month":OPS_A[i]["month"], "ops": OPS_growth}) #ops_y 리스트에 추가
+    OPS_growth = OPS_A[i].get("ops") - OPS_f #한 경기 OPS - 맨 처음경기 OPS
+    ops_y.append({"year":OPS_A[i].get("year"), "month":OPS_A[i].get("month"), "ops": OPS_growth}) #ops_y 리스트에 추가
 
 print ops_y
 
